7c175af59451c8034c158cda6c208bafd5f2d48d
Imadlmair <116261917+Imadlmair@users.noreply.github.com>
2022-12-01 12:37:42 +0100

    Add files via upload

---
+++ Halistone-Sequence.cs
@@ -0,0 +1,21 @@
+﻿int i = 0;
+int newnumb = 0;
+System.Console.WriteLine("Please enter the number you want to start with ? " );
+int startnumb = int.Parse(Console.ReadLine()!);
+do{
+    
+    if(startnumb % 2 == 0)
+    {
+        i ++;
+        newnumb = startnumb / 2;
+    }
+    
+    else 
+    {
+        newnumb = startnumb * 3 + 1;
+        i++;
+    }
+    startnumb = newnumb;
+}while(startnumb != 1);
+i ++;
+System.Console.WriteLine($"The lenght of your sequence is {i}");
